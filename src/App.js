edbd7778bcf37cda0b9d0fe200ed112bb8ecc45a
deedeeh <dinahafez.8@gmail.com>
2019-06-04 00:00:27 +0100

    Start with mobile first design and add header and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
-import "./App.css";
+// Third party dependencies
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+// relative paths dependencies
+import './App.css';
+import headerBg from './assets/images/header_bg.png';
+import logo from './assets/images/MusicPlay_logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <header className="App_header">
+        <Image
+          className="header_img"
+          src={headerBg}
+          alt="Black strips on a cyan background header image."
+        />
+        <Image className="logo" src={logo} alt="Music play word logo" />
+      </header>
     </div>
   );
 }
