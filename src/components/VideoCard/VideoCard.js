c11ec08490083be083ba73b791fdb746a89f2815
deedeeh <dinahafez.8@gmail.com>
2019-06-10 01:04:30 +0100

    Add the correct date format and update the font family for it

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -7,12 +7,17 @@ import './VideoCard.css';
 
 const VideoCard = (props) => {
   return (
-    props.videosList.map(video => 
-      <List.Item key={video.contentDetails.videoId}>
+    props.videosList.map(video => {
+      const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+      const month = new Date(video.snippet.publishedAt).getMonth();
+      const getTheMonthAbbr = months[month]
+      const getTheday = new Date(video.snippet.publishedAt).getDate();
+      const getTheyear = new Date(video.snippet.publishedAt).getFullYear();
+      return <List.Item key={video.contentDetails.videoId}>
         <Image src={video.snippet.thumbnails.high.url} />
-        <h2 className='video_header'>{video.snippet.title}</h2>
+        <h3 className='video_header'>{video.snippet.title}</h3>
         <List.Content>
-          <h4 className="video_date">Published on {video.snippet.publishedAt}</h4>
+          <h4 className="video_date">Published on {`${getTheMonthAbbr} ${getTheday}, ${getTheyear}`}</h4>
           {
             video.snippet.description === ""
           ? <p className='video_content'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
@@ -20,7 +25,7 @@ const VideoCard = (props) => {
           }
         </List.Content>
       </List.Item>
-    )
+    })
   )
 }
 
